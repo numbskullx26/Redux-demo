992d253c5a4744a31beb226255f7bc55b055a0a9
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-06 13:18:47 +0530

    first action and action creator

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("From Index.js");
+// A constant defining the state of the variable
+const CAKE_ORDERED = "CAKE_ORDERED";
+// First action in Redux
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
