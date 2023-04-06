8625533d2d6a8ca7a544838bdc998f58ce1bda2a
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-06 13:20:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ console.log("From Index.js");
 // A constant defining the state of the variable
 const CAKE_ORDERED = "CAKE_ORDERED";
 // First action in Redux
+// An action is an object with the type property
+// An action creator is an pure function that returns the object
 function orderCake() {
   return {
     type: CAKE_ORDERED,
