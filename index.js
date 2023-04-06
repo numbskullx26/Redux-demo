f9ab0837ea7e4e0ba98ac91ef7636f45908b9d1b
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-06 13:58:55 +0530

    Implementing the whole redux store functionality

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-console.log("From Index.js");
+const redux = require("redux");
+// Using redux to create a new store to implement the following functions of a redux store
+
+const createStore = redux.createStore;
+
 // A constant defining the state of the variable
 const CAKE_ORDERED = "CAKE_ORDERED";
 // First action in Redux
@@ -30,3 +34,17 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial State", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
